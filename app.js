60c4cb02d8e2411019b10fc58b2f403fea67d63f
moonhee0507 <sjmh0507@naver.com>
2022-08-11 00:35:21 +0900

    📝 경로 설정을 위한 코드

---
+++ app.js
@@ -0,0 +1,22 @@
+import { Home, Login, Signup } from "./js/pages/index.js";
+import { Router } from "./utils/index.js";
+
+export default class App {
+    constructor(props) {
+        this.props = props;
+    }
+    // index.js의 config가 여기로 들어온다
+    setup() {
+        const { el } = this.props;
+
+        // 미리 경로를 정해 놓는 곳
+        const router = new Router({
+            "/": Home,
+            "/login": Login,
+            "/signup": Signup,
+        });
+
+        // root에 띄워준다
+        router.init(el);
+    }
+}
