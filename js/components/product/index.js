235ca5fa4da0060a2049af56cc60109e20ba5da6
moonhee0507 <sjmh0507@naver.com>
2022-08-16 23:05:31 +0900

    🚚 Shipping 통합 및 Total 컴포넌트 추가

--- js/components/product/index.js
+++ js/components/product/index.js
@@ -2,5 +2,6 @@ export { default as ProductImage } from "./productImage.js";
 export { default as ProductSeller } from "./productSeller.js";
 export { default as ProductName } from "./productName.js";
 export { default as ProductPrice } from "./productPrice.js";
-export { default as ProductShippingMethod } from "./productShippingMethod.js";
-export { default as ProductShippingFee } from "./productShippingFee.js";
+export { default as ProductShipping } from "./productShipping.js";
+export { default as ProductQuantity } from "./productQuantity.js";
+export { default as ProductTotal } from "./productTotal.js";
