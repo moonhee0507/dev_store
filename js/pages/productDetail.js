427186d5a4b5c9c1dc8dc0a578f86eaa5f5041cf
moonhee0507 <sjmh0507@naver.com>
2022-08-18 00:05:00 +0900

    ✨ ProductTab 및 Footer 컴포넌트 추가

--- js/pages/productDetail.js
+++ js/pages/productDetail.js
@@ -1,15 +1,20 @@
 import Header from "../components/header/header.js";
-import ProductInfo from "../components/productInfo/productInfo.js";
+import { ProductInfo, ProductTab } from "../components/productInfo/index.js";
+import Footer from "../components/footer/footer.js";
 
 class ProductDetail {
-    constructor(header, productInfo) {
+    constructor(header, productInfo, productTab, footer) {
         this.header = header;
         this.productInfo = productInfo;
+        this.productTab = productTab;
+        this.footer = footer;
     }
 
     render() {
         this.header = new Header();
         this.productInfo = new ProductInfo();
+        this.productTab = new ProductTab();
+        this.footer = new Footer();
 
         const header = document.createElement("header");
         header.setAttribute("class", "header");
@@ -18,8 +23,13 @@ class ProductDetail {
         const main = document.createElement("main");
         main.setAttribute("class", "max-width");
         main.appendChild(this.productInfo.render());
+        main.appendChild(this.productTab.render());
 
-        document.getElementById("root").append(header, main);
+        const footer = document.createElement("footer");
+        footer.setAttribute("class", "footer");
+        footer.appendChild(this.footer.render());
+
+        document.getElementById("root").append(header, main, footer);
     }
 }
 
