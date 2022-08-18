a1c04beb394ed9b573c7c59d27fcebc4f896aca0
moonhee0507 <sjmh0507@naver.com>
2022-08-19 01:01:02 +0900

    ✨ 로그인 안내 모달 컴포넌트 연결

--- js/pages/productDetail.js
+++ js/pages/productDetail.js
@@ -1,12 +1,14 @@
 import Header from "../components/header/header.js";
 import { ProductInfo, ProductTab } from "../components/productInfo/index.js";
+import { GoToLogin } from "../components/modal/index.js";
 import Footer from "../components/footer/footer.js";
 
 class ProductDetail {
-    constructor(header, productInfo, productTab, footer) {
+    constructor(header, productInfo, productTab, goToLogin, footer) {
         this.header = header;
         this.productInfo = productInfo;
         this.productTab = productTab;
+        this.goToLogin = goToLogin;
         this.footer = footer;
     }
 
@@ -14,6 +16,7 @@ class ProductDetail {
         this.header = new Header();
         this.productInfo = new ProductInfo();
         this.productTab = new ProductTab();
+        this.goToLogin = new GoToLogin();
         this.footer = new Footer();
 
         const header = document.createElement("header");
@@ -24,6 +27,7 @@ class ProductDetail {
         main.setAttribute("class", "max-width");
         main.appendChild(this.productInfo.render());
         main.appendChild(this.productTab.render());
+        main.appendChild(this.goToLogin.render());
 
         const footer = document.createElement("footer");
         footer.setAttribute("class", "footer");
