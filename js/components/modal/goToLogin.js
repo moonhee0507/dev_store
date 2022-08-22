c5d9193b12d706ceb57fa0221ec780daaebd342d
moonhee0507 <sjmh0507@naver.com>
2022-08-23 01:47:42 +0900

    ✨ 로그인 안내 모달 클릭별 이벤트 구현

--- js/components/modal/goToLogin.js
+++ js/components/modal/goToLogin.js
@@ -35,6 +35,31 @@ class GoToLogin {
         modalContent.append(buttonClose, modalWrapperTxt, modalWrapperbutton);
         this.modal.appendChild(modalContent);
 
+        const body = document.querySelector("body");
+
+        // 바깥부분 클릭하면 모달창이 없어지는 기능
+        this.modal.addEventListener("click", (e) => {
+            const target = e.target;
+            if (target.classList.contains("show")) {
+                target.classList.remove("show");
+                body.style.overflow = "auto";
+            }
+        });
+        // x 버튼 클릭하면 모달창이 없어지는 기능
+        buttonClose.addEventListener("click", () => {
+            this.modal.classList.remove("show");
+            body.style.overflow = "auto";
+        });
+        // 아니오 버튼 누르면 모달창이 없어지는 기능
+        buttonNo.addEventListener("click", () => {
+            this.modal.classList.remove("show");
+            body.style.overflow = "auto";
+        });
+        // 예 버튼을 누르면 로그인 페이지로 이동하는 기능
+        buttonYes.addEventListener("click", () => {
+            window.location.pathname = "/login";
+        });
+
         return this.modal;
     }
 }
