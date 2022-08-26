25a6be7575bb4158cc3bdc56b5e864d7b54ced5e
moonhee0507 <sjmh0507@naver.com>
2022-08-27 02:00:56 +0900

    ✏️ 주문/결제하기 페이지의 form 컴포넌트 index화

---
+++ js/components/ordererInfo/index.js
@@ -0,0 +1,3 @@
+export { default as OrdererName } from "./ordererName.js";
+export { default as OrdererPhone } from "./ordererPhone.js";
+export { default as OrdererEmail } from "./ordererEmail.js";
---
+++ js/components/recipientInfo/index.js
@@ -0,0 +1,4 @@
+export { default as RecipientName } from "./recipientName.js";
+export { default as RecipientPhone } from "./recipientPhone.js";
+export { default as RecipientAddress } from "./recipientAddress.js";
+export { default as ShippingMessage } from "./shippingMessage.js";
