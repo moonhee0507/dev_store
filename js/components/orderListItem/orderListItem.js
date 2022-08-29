4cfe2aff3d84b99cc5099d2cac18f4feb8a37ee8
moonhee0507 <sjmh0507@naver.com>
2022-08-29 19:22:50 +0900

    🔥 주문상품 상세정보 요청 코드 삭제

--- js/components/orderListItem/orderListItem.js
+++ js/components/orderListItem/orderListItem.js
@@ -1,56 +1,50 @@
-import { API_URL } from "../../common/constants.js";
+import { ProductShipping } from "../product/index.js";
 import { ProductInfoCard } from "../productInfoCard/index.js";
 
 class OrderListItem {
-    constructor(item) {
-        this.item = item;
+    constructor() {
         this.wrapper = document.createElement("div");
-        this.detail = {};
     }
 
-    // 주문 상품 상세정보 가져오기
-    async getProductData() {
-        const res = await fetch(
-            `${API_URL}/products/${this.item.order_items}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-        const data = await res.json();
-
-        this.detail = await data;
-    }
-
-    // 주문상품 세팅하기
-    async setOrder() {
-        await this.getProductData();
-
+    render() {
         const wrapperPaymentProduct = document.createElement("div");
         wrapperPaymentProduct.setAttribute("class", "wrapper-payment-product");
 
         // 상품정보 열
         const paymentProductInfoCol = document.createElement("div");
         paymentProductInfoCol.setAttribute("class", "payment-product-info-col");
-        const productInfoCard = new ProductInfoCard(
-            this.detail,
-            this.item.order_quantity
-        );
+        const productInfoCard = new ProductInfoCard();
         paymentProductInfoCol.appendChild(productInfoCard.render());
 
         // 할인 열
         const paymentDiscountCol = document.createElement("div");
         paymentDiscountCol.setAttribute("class", "payment-discount-col");
+        paymentDiscountCol.innerHTML = `
+            <p class="payment-txt-discount">-</p>
+        `;
 
         // 배송비 열
         const paymentShippingCol = document.createElement("div");
         paymentShippingCol.setAttribute("class", "payment-shipping-col");
+        const productShipping = new ProductShipping();
+        paymentShippingCol.appendChild(productShipping.render());
 
         // 주문금액 열
         const paymentOrderAmountCol = document.createElement("div");
         paymentOrderAmountCol.setAttribute("class", "payment-order-amount-col");
+        const paymentTxtAmount = document.createElement("p");
+        paymentTxtAmount.setAttribute("class", "payment-txt-amount");
+        const paymentNumAmount = document.createElement("span");
+        paymentNumAmount.setAttribute("class", "payment-num-amount");
+        paymentNumAmount.innerText =
+            Number(
+                JSON.parse(window.localStorage.getItem("fromDetail"))
+                    .selectedTotal
+            ).toLocaleString("ko-KR") + "원";
+
+        console.log();
+        paymentTxtAmount.appendChild(paymentNumAmount);
+        paymentOrderAmountCol.appendChild(paymentTxtAmount);
 
         wrapperPaymentProduct.append(
             paymentProductInfoCol,
@@ -59,10 +53,6 @@ class OrderListItem {
             paymentOrderAmountCol
         );
         this.wrapper.appendChild(wrapperPaymentProduct);
-    }
-
-    render() {
-        this.setOrder();
 
         return this.wrapper;
     }
