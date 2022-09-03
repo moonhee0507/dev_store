1e5d9d5bf0c1243dff53b9299b9e07f6d767908d
moonhee0507 <sjmh0507@naver.com>
2022-09-04 01:26:17 +0900

    💄 대시보드 메뉴 구현

---
+++ js/components/dashboardMenu/dashboardMenu.js
@@ -0,0 +1,77 @@
+class DashboardMenu {
+    constructor() {
+        this.menu = document.createElement("div");
+    }
+
+    render() {
+        this.menu.setAttribute("class", "center-menu");
+
+        const ul = document.createElement("ul");
+        ul.setAttribute("class", "center-list");
+
+        let listLength = 5;
+        let listArr = Array();
+
+        // 판매중인 상품
+        const txtOnsale = document.createElement("div");
+        txtOnsale.setAttribute("class", "txt-onsale");
+        const numOnsale = document.createElement("span");
+        numOnsale.setAttribute("class", "num-onsale");
+        numOnsale.innerText = 3;
+        txtOnsale.append("판매중인 상품(", numOnsale, ")");
+
+        listArr.push(txtOnsale);
+
+        // 주문/배송
+        const txtOrderDelivery = document.createElement("div");
+        txtOrderDelivery.setAttribute("class", "txt-order-delivery");
+        txtOrderDelivery.innerText = "주문/배송";
+        const numSmallOd = document.createElement("span");
+        numSmallOd.setAttribute("class", "num-small od");
+        const numOrderDelivery = document.createElement("em");
+        numOrderDelivery.innerText = "2";
+        numSmallOd.appendChild(numOrderDelivery);
+
+        listArr.push(txtOrderDelivery);
+
+        // 문의/리뷰
+        const txtInquiryReview = document.createElement("div");
+        txtInquiryReview.setAttribute("class", "txt-inquiry-review");
+        txtInquiryReview.innerText = "문의/리뷰";
+        const numSmallIr = document.createElement("span");
+        numSmallIr.setAttribute("class", "num-small ir");
+        const numInquiryReview = document.createElement("em");
+        numInquiryReview.innerText = "1";
+        numSmallIr.appendChild(numInquiryReview);
+
+        listArr.push(txtInquiryReview);
+
+        // 통계
+        const txtStatistics = document.createElement("div");
+        txtStatistics.setAttribute("class", "txt-statistics");
+        txtStatistics.innerText = "통계";
+
+        listArr.push(txtStatistics);
+
+        // 스토어 설정
+        const txtStoreSettings = document.createElement("div");
+        txtStoreSettings.setAttribute("class", "txt-store-settings");
+        txtStoreSettings.innerText = "스토어 설정";
+
+        listArr.push(txtStoreSettings);
+
+        this.menu.appendChild(ul);
+
+        for (let i = 0; i < listLength; i++) {
+            const li = document.createElement("li");
+            li.appendChild(listArr[i]);
+            ul.appendChild(li);
+        }
+        txtOrderDelivery.after(numSmallOd);
+        txtInquiryReview.after(numSmallIr);
+
+        return this.menu;
+    }
+}
+
+export default DashboardMenu;
