13456c270e6750a2b092eee9b39abb06cf2c8ef0
moonhee0507 <sjmh0507@naver.com>
2022-09-07 02:22:47 +0900

    ✨ 상품등록 페이지 컴포넌트화

---
+++ js/components/uploadForm/index.js
@@ -0,0 +1,6 @@
+export { default as UploadImage } from "./uploadImage.js";
+export { default as UploadProductName } from "./uploadProductName.js";
+export { default as UploadPrice } from "./uploadPrice.js";
+export { default as UploadShippingMethod } from "./uploadShippingMethod.js";
+export { default as UploadShippingFee } from "./uploadShippingFee.js";
+export { default as UploadStock } from "./uploadStock.js";
