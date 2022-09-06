1815410e607692f5e31b05ab1a334a55581ae380
moonhee0507 <sjmh0507@naver.com>
2022-09-07 02:22:16 +0900

    ✨ 대시보드 셀러 이름 추가

--- js/components/dashboardTitle/dashboardTitle.js
+++ js/components/dashboardTitle/dashboardTitle.js
@@ -1,11 +1,27 @@
+import { API_URL } from "../../common/constants.js";
 import ProductUploadButton from "../button/productUploadButton.js";
 
 class DashboardTitle {
     constructor() {
         this.container = document.createElement("div");
+        this.store = {};
     }
 
-    render() {
+    async getSellerName() {
+        const res = await fetch(`${API_URL}/seller/`, {
+            method: "GET",
+            headers: {
+                Authorization: `JWT ${window.localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await res.json();
+        this.store = await data.results[0].seller_store;
+    }
+
+    async setSellerName() {
+        await this.getSellerName();
+
         this.container.setAttribute("class", "style-container-center-top");
 
         const containerSeller = document.createElement("div");
@@ -15,16 +31,22 @@ class DashboardTitle {
         );
 
         const title = document.createElement("h3");
+        title.setAttribute("class", "center-title3");
         title.innerText = "대시보드";
 
         const centerTxtSeller = document.createElement("strong");
         centerTxtSeller.setAttribute("class", "center-txt-seller");
-        centerTxtSeller.innerText = "백엔드 글로벌";
+        centerTxtSeller.innerText = this.store;
 
         const productUploadButton = new ProductUploadButton();
 
         containerSeller.append(title, centerTxtSeller);
         this.container.append(containerSeller, productUploadButton.render());
+    }
+
+    render() {
+        this.setSellerName();
+
         return this.container;
     }
 }
