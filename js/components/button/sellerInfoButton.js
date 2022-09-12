5d958b9290bd85bcff4bbb15a697300814d4e91f
moonhee0507 <sjmh0507@naver.com>
2022-09-12 19:57:22 +0900

    💄 판매자 상세정보 버튼 컴포넌트화

---
+++ js/components/button/sellerInfoButton.js
@@ -0,0 +1,15 @@
+class SellerInfoButton {
+    constructor(seller) {
+        this.seller = seller;
+        this.button = document.createElement("button");
+    }
+
+    render() {
+        this.button.setAttribute("class", "button-seller-info");
+        this.button.innerText = "정보";
+
+        return this.button;
+    }
+}
+
+export default SellerInfoButton;
