4d0cee597048750132ebbdc6d7473bc603e710d2
moonhee0507 <sjmh0507@naver.com>
2022-09-14 00:11:23 +0900

    💄 낮은가격순, 높은가격순 정렬 클릭 이벤트 스타일 추가

--- js/components/sortOrder/sortOrder.js
+++ js/components/sortOrder/sortOrder.js
@@ -6,21 +6,47 @@ class SortOrder {
     render() {
         this.div.setAttribute("class", "sort-order");
 
-        const lowPriceOrder = document.createElement("a");
+        const lowPriceOrder = document.createElement("button");
+        lowPriceOrder.setAttribute("type", "button");
         lowPriceOrder.setAttribute("class", "sort low-price");
         lowPriceOrder.innerText = "낮은 가격순";
 
-        const highPriceOrder = document.createElement("a");
+        const highPriceOrder = document.createElement("button");
+        highPriceOrder.setAttribute("type", "button");
         highPriceOrder.setAttribute("class", "sort high-price");
         highPriceOrder.innerText = "높은 가격순";
 
-        const createdOrder = document.createElement("a");
+        const createdOrder = document.createElement("button");
+        createdOrder.setAttribute("type", "button");
         createdOrder.setAttribute("class", "sort created");
         createdOrder.innerText = "등록일순";
 
         this.div.appendChild(lowPriceOrder);
         this.div.appendChild(highPriceOrder);
         this.div.appendChild(createdOrder);
+
+        let arrButton = [lowPriceOrder, highPriceOrder, createdOrder];
+        for (let i = 0; i < arrButton.length; i++) {
+            arrButton[i].addEventListener("click", (e) => {
+                initialize();
+                let target = e.target;
+                colorize(target);
+            });
+        }
+
+        function colorize(target) {
+            target.style.color = "#a158fe";
+            target.classList.add("on");
+            target.disabled = true;
+        }
+
+        function initialize() {
+            for (let i = 0; i < arrButton.length; i++) {
+                arrButton[i].style.color = "rgb(233, 233, 233)";
+                arrButton[i].classList.remove("on");
+                arrButton[i].disabled = false;
+            }
+        }
 
         return this.div;
     }
