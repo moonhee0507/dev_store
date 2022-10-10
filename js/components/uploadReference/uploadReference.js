9992bbac8012d33a0e43aaebc8ddd47a72f7339d
moonhee0507 <sjmh0507@naver.com>
2022-10-11 00:48:16 +0900

    💬 상품상세정보 안내문구 수정

--- js/components/uploadReference/uploadReference.js
+++ js/components/uploadReference/uploadReference.js
@@ -18,8 +18,7 @@ class UploadReference {
         const paragragh1 =
             "- 상품 이미지\n.jpg, .jpeg, .png 파일만 업로드 가능합니다. 크기는 1000px * 1000px(정사각형) 권장합니다. 업로드가 안될 시 파일명에 특수문자를 제거하고 다시 시도해주세요.";
         const paragragh2 = "- 상품명\n4~20자 이내로 제한됩니다.";
-        const paragragh3 =
-            "- 상품 상세 정보를 입력하는 에디터 영역 기능이 있습니다.";
+        const paragragh3 = "- 상품 상세 정보(필수)";
         const paragragh4 =
             "- 저장하기 버튼을 누르면 상품 등록이 완료되며 상품 상세 페이지로 이동합니다.";
 
