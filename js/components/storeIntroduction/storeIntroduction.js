583379c45b684fbe1f2276adbbc1608eb34a205a
moonhee0507 <sjmh0507@naver.com>
2022-11-16 00:19:11 +0900

    💄 판매자 스토어명 반응형 처리

--- js/components/storeIntroduction/storeIntroduction.js
+++ js/components/storeIntroduction/storeIntroduction.js
@@ -1,36 +1,37 @@
-import { API_URL } from "../../common/constants.js";
+import getProducts from "../../common/api.js";
+import SellerNameCard from "./sellerNameCard.js";
 
 class StoreIntroduction {
     constructor() {
         this.div = document.createElement("div");
+        this.h2 = document.createElement("h2");
+        this.products = {};
+        this.productsOnAllPages = [];
+    }
+
+    async getProductsData() {
+        let count = parseInt(window.localStorage.getItem("count"));
+        for (let i = 1; i < count / 15 + 1; i++) {
+            const data = await getProducts(i, false);
+            this.products = await data.results;
+            this.productsOnAllPages.push(...this.products);
+        }
+        this.h2.setAttribute("class", "title-store");
+        const url = window.location.pathname;
+        let sellerNumber = parseInt(url.replace(/^\/store\//g, ""));
+        const firstElement = this.productsOnAllPages.find((el) => {
+            return el.seller === sellerNumber;
+        });
+        this.h2.innerText = firstElement.store_name;
+        const sellerNameCard = new SellerNameCard(firstElement.store_name);
+        const root = document.getElementById("root");
+        root.appendChild(sellerNameCard.render());
     }
 
     render() {
+        this.getProductsData();
         this.div.setAttribute("class", "style-wrapper-store-introduction");
-
-        const h2 = document.createElement("h2");
-        h2.setAttribute("class", "title-store");
-        const url = window.location.pathname;
-        let sellerNumber = parseInt(url.replace(/^\/store\//g, ""));
-        this.div.appendChild(h2);
-
-        findSellerName();
-        async function findSellerName() {
-            await fetch(`${API_URL}/products/`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-                .then((res) => res.json())
-                .then((data) => {
-                    const firstElement = data.results.find((el) => {
-                        return el.seller === sellerNumber;
-                    });
-                    h2.innerText = firstElement.store_name;
-                })
-                .catch((e) => console.error(e));
-        }
+        this.div.appendChild(this.h2);
 
         return this.div;
     }
