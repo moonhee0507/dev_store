163bd913b6ffe25b4e45473381e54d2a07094e46
moonhee0507 <sjmh0507@naver.com>
2023-01-03 00:24:05 +0900

    🐛 @aws-sdk/- 외부화

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from "vite";
 
-const outputPluginStats = () => ({
-    name: "output-plugin-stats",
-    configResolved(config) {
-        const plugins = config.plugins.map((plugin) => plugin.name);
-        console.log(`Your project has ${plugins.length} Vite plugins.`);
-        console.table(plugins);
-    },
-});
-
 export default defineConfig({
     resolve: {
         alias: {
